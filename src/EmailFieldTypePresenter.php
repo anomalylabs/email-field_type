d6e970e1fc010adba06abed659eccef1afc21af9
Aidan Watt <aidan@hotfootdesign.co.uk>
2025-10-19 12:47:55 +0100

    Laravel 12 work

--- src/EmailFieldTypePresenter.php
+++ src/EmailFieldTypePresenter.php
@@ -1,7 +1,7 @@
 <?php namespace Anomaly\EmailFieldType;
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
-use Collective\Html\HtmlBuilder;
+use Anomaly\Streams\Platform\Html\HtmlBuilder;
 
 /**
  * Class EmailFieldTypePresenter
